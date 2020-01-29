c64918a4907c7fd0b673e38a898db1d106b568d4
Jenny Gardner <j.rongjie@gmail.com>
2020-01-29 18:22:52 -0500

    completed lab3

--- lab3/index.js
+++ lab3/index.js
@@ -1,16 +1,16 @@
+//Create a express server()
 const Express = require("express");
 const App = Express();
-const port = 8080;
+const port = 80;
 
-//doesnt work
-App.get("/", (req, res) => {
-    res.send("Hello World");
+//Create a route ("/") that sends the string "Hello World!"
+App.get("/", function(req, res){
+    res.send("Hello World!");
 });
 
-App.get("/public", (req, res) => {
-    res.send("./public/index.html");
-});
+//Create a route ("/public") that serves file from the public directory
+App.use("/public", Express.static("public"));
 
-App.listen(port, function(){
+App.listen(port, () => {
     console.log ("Server running");
 });
