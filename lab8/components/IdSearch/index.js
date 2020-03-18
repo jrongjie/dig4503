5901cd9a0db179736c2ed9fce62e5566e90ee2e5
Jenny Gardner <j.rongjie@gmail.com>
2020-03-18 16:06:56 -0400

    copied search

---
+++ lab8/components/IdSearch/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class IdSearch extends React.Component {
+    idGrabber(event){
+        event.preventDefault();  
+        
+        let formId = document.querySelector("#pokeId");
+                    
+        fetch("http://localhost:80/id/" + formId.value).then((res) => {
+            return res.json();
+        
+        }).then((processed) => {
+            let reporting = document.querySelector("#reportingArea");
+                
+            if(processed.error) {
+                reporting.innerHTML = processed.error;
+            } else {
+                reporting.innerHTML = "So you are a " + processed.name + "! I knew it!";
+            }
+        });
+    };
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.idGrabber}>
+                    <h1>Give me your ID</h1>
+                    <input type="text" id="pokeId"/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    };
+}
+
+export default IdSearch;
