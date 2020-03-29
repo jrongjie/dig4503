41adbeb3be2d3e3293bce7e7795d213b023102b0
Jenny Gardner <j.rongjie@gmail.com>
2020-03-28 20:25:14 -0400

    Started styles

--- lab9/pages/index.js
+++ lab9/pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
-//import Style from './styles.module.css'
+import Style from '../components/Style.module.css'
 
-class Pokemon extends React.Component{
-
-  render(){
+const Pokemon = () => {
     return(
-        <div>
+        <div className={Style.general}>
             <Head>
                 <title>Pokemon</title>
             </Head>
@@ -14,19 +12,16 @@ class Pokemon extends React.Component{
             <h1>Welcome to the Pokemon shelter!</h1>
             <p>Find a pokemon companion today...</p>
             <Link href="/id">
-                <a>Search by ID</a>
+                <a className={Style.links}>Search by ID</a>
             </Link>
-                <br/>
             <Link href="/name">
-                <a>Search by Name</a>
+                <a className={Style.links}>Search by Name</a>
             </Link>
-                <br/>
             <Link href="/type">
-                <a>Search by Type</a>
+                <a className={Style.links}>Search by Type</a>
             </Link>
         </div>
     )
-  }
 }
 
 export default Pokemon;
