3830e343ff0a80472e227d7cb4e7efa55bc5edd4
Jenny Gardner <j.rongjie@gmail.com>
2020-04-15 20:23:28 -0400

    Copied Style and TypeSearch component from lab9

--- final-project/client/src/App.js
+++ final-project/client/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import IdSearch from './components/IdSearch';
-import NameSearch from './components/NameSearch';
-import ReportingArea from './components/ReportingArea';
+import React from "react";
+import IdSearch from "./components/IdSearch";
+import NameSearch from "./components/NameSearch";
+//import TypeSearch from "./components/TypeSearch";
+import ReportingArea from "./components/ReportingArea";
 
 function App() {
   return (
     <div>
-      <IdSearch></IdSearch>
-      <NameSearch></NameSearch>
-      <ReportingArea></ReportingArea>
+      <IdSearch/>
+      <NameSearch/>
+      <ReportingArea/>
     </div>
   );
 }
