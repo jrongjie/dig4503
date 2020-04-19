7b6d8f8d59e84d17667e5b4b0714e1d376dad216
Jenny Gardner <j.rongjie@gmail.com>
2020-04-18 20:13:02 -0400

    Revert "Copied Style and TypeSearch component from lab9"
    This reverts commit 3830e343ff0a80472e227d7cb4e7efa55bc5edd4.

---
+++ final-project/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import IdSearch from './components/IdSearch';
+import NameSearch from './components/NameSearch';
+import ReportingArea from './components/ReportingArea';
+
+function App() {
+  return (
+    <div>
+      <IdSearch></IdSearch>
+      <NameSearch></NameSearch>
+      <ReportingArea></ReportingArea>
+    </div>
+  );
+}
+
+export default App;
